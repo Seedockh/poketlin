18b5ba274cb508ebe78765a96ff23da3f4ba57b9
pierr <pierre.herisse@gmail.com>
2020-06-13 23:54:25 +0200

    Work on UI

--- app/src/main/java/com/pierreherisse/poketlin/pokedex/PokedexAdapter.kt
+++ app/src/main/java/com/pierreherisse/poketlin/pokedex/PokedexAdapter.kt
@@ -15,20 +15,24 @@ class PokedexAdapter : RecyclerView.Adapter<PokedexAdapter.PokemonViewHolder>() 
     var pokedex: List<Pokemon> by Delegates.observable(emptyList()) { _, _, _ ->
         notifyDataSetChanged()
     }
+    var onItemClick: ((String) -> Unit)? = { }
 
     inner class PokemonViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         fun bind(pokemon: Pokemon) {
             var pokemonUrl = pokemon.url.split("/")
-            var imageEndpoint = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonUrl[pokemonUrl.lastIndex-1]}.png"
+            var pokemonId = pokemonUrl[pokemonUrl.lastIndex-1]
+            var imageEndpoint = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonId}.png"
+
+
             Picasso.get().load(imageEndpoint).into(itemView.pokemonImage);
 
             itemView.pokemonName.text = pokemon.name.capitalize()
+            itemView.setOnClickListener { onItemClick?.invoke("$pokemonId") }
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PokemonViewHolder {
-        val itemView =
-            LayoutInflater.from(parent.context).inflate(R.layout.item_pokemon, parent, false)
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.item_pokemon, parent, false)
         return PokemonViewHolder(itemView)
     }
 
--- app/src/main/java/com/pierreherisse/poketlin/pokedex/PokedexFragment.kt
+++ app/src/main/java/com/pierreherisse/poketlin/pokedex/PokedexFragment.kt
@@ -1,18 +1,19 @@
 package com.pierreherisse.poketlin.pokedex
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.pierreherisse.poketlin.R
-import com.pierreherisse.poketlin.pokemon.Pokemon
+import com.pierreherisse.poketlin.pokemon.PokemonActivity
 import kotlinx.android.synthetic.main.fragment_pokedex.*
+import java.io.Serializable
 
 class PokedexFragment : Fragment() {
 
@@ -31,12 +32,25 @@ class PokedexFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        recyclerView.addItemDecoration(
+            MarginItemDecoration(
+                resources.getDimension(R.dimen.default_padding).toInt()
+            )
+        )
+
         recyclerView.adapter = adapter
         recyclerView.layoutManager = LinearLayoutManager(activity)
 
         viewModelPokemon.pokedex.observe(this, Observer { newList ->
             adapter.pokedex = newList.orEmpty()
         })
+
+        adapter.onItemClick = { id ->
+            val pokemonIntent = Intent(recyclerView.context, PokemonActivity::class.java)
+            pokemonIntent.putExtra("id", id as Serializable)
+            startActivityForResult(pokemonIntent, 200)
+        }
     }
 
     override fun onResume() {
